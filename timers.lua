acace094b0729948ec01d29a7927424a45ec6ac1
Daniel Augusto <danisson10@gmail.com>
2017-01-21 01:48:20 +0000

    Timers

---
+++ timers.lua
@@ -0,0 +1,66 @@
+-- creates and runs timers
+-- also supports pausing and resuming
+-- full info: http://www.lexaloffle.com/bbs/?tid=3202
+
+-- Contributors: BenWiley4000
+
+local timers = {}
+local last_time = nil
+
+function init_timers ()
+  last_time = time()
+end
+
+function add_timer (name,
+    length, step_fn, end_fn,
+    start_paused)
+  local timer = {
+    length=length,
+    elapsed=0,
+    active=not start_paused,
+    step_fn=step_fn,
+    end_fn=end_fn
+  }
+  timers[name] = timer
+  return timer
+end
+
+function update_timers ()
+  local t = time()
+  local dt = t - last_time
+  last_time = t
+  for name,timer in pairs(timers) do
+    if timer.active then
+      timer.elapsed += dt
+      local elapsed = timer.elapsed
+      local length = timer.length
+      if elapsed < length then
+        if timer.step_fn then
+          timer.step_fn(dt,elapsed,length,timer)
+        end
+    else
+        timer.active = false
+        if timer.end_fn then
+          timer.end_fn(dt,elapsed,length,timer)
+        end
+      end
+    end
+  end
+end
+
+function pause_timer (name)
+  local timer = timers[name]
+  if (timer) timer.active = false
+end
+
+function resume_timer (name)
+  local timer = timers[name]
+  if (timer) timer.active = true
+end
+
+function restart_timer (name, start_paused)
+  local timer = timers[name]
+  if (not timer) return
+  timer.elapsed = 0
+  timer.active = not start_paused
+end
